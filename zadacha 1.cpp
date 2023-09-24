7ea7993ee8559ddb3fb83c40d2fc9f2854cb704d
Monobrov472 <144647756+Monobrov472@users.noreply.github.com>
2023-09-24 17:50:26 +0300

    Add files via upload

---
+++ zadacha 1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    // Оголошуємо змінні для температур у обох шкалах
+    float TC, TF;
+
+    // Запитуємо користувача ввести температуру у Цельсіях
+    cout << "Enter the temperature in degrees Celsius: ";
+    cin >> TC;
+
+    // Конвертуємо температуру у Фаренгейти
+    TF = (TC * 9 / 5) + 32;
+
+    // Виводимо результат
+    cout << "Temperature in degrees Fahrenheit: " << TF << endl;
+
+    return 0;
+}
